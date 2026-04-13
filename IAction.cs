b35588d977af89bd259f8df47791de8aed82b2b6
alast0r <efanov.vladisla6@gmail.com>
2026-04-13 22:49:24 +0300

    Увеличение функций

--- IAction.cs
+++ IAction.cs
@@ -45,3 +45,59 @@ public class DelayAction : IAction
     public string Description => "Задержка: " + _seconds + " сек";
     public string ToBatCommand() => "timeout /t " + _seconds;
 }
+
+public class CopyFileAction : IAction
+{
+    private readonly string _source;
+    private readonly string _destination;
+    public CopyFileAction(string source, string destination) { _source = source; _destination = destination; }
+    public string Description => "Копировать файл: " + _source + " -> " + _destination;
+    public string ToBatCommand() => "copy \"" + _source + "\" \"" + _destination + "\"";
+}
+
+public class MoveFileAction : IAction
+{
+    private readonly string _source;
+    private readonly string _destination;
+    public MoveFileAction(string source, string destination) { _source = source; _destination = destination; }
+    public string Description => "Переместить файл: " + _source + " -> " + _destination;
+    public string ToBatCommand() => "move \"" + _source + "\" \"" + _destination + "\"";
+}
+
+public class DeleteFileAction : IAction
+{
+    private readonly string _filePath;
+    public DeleteFileAction(string filePath) { _filePath = filePath; }
+    public string Description => "Удалить файл: " + _filePath;
+    public string ToBatCommand() => "del /F /Q \"" + _filePath + "\"";
+}
+
+public class CreateFolderAction : IAction
+{
+    private readonly string _folderPath;
+    public CreateFolderAction(string folderPath) { _folderPath = folderPath; }
+    public string Description => "Создать папку: " + _folderPath;
+    public string ToBatCommand() => "mkdir \"" + _folderPath + "\"";
+}
+
+public class ListProcessesAction : IAction
+{
+    public ListProcessesAction() { }
+    public string Description => "Список процессов";
+    public string ToBatCommand() => "tasklist";
+}
+
+public class ShutdownAction : IAction
+{
+    private readonly int _minutes;
+    public ShutdownAction(int minutes = 0) { _minutes = minutes; }
+    public string Description => "Выключить ПК через " + _minutes + " мин";
+    public string ToBatCommand() => "shutdown /s /t " + (_minutes * 60);
+}
+
+public class ClearScreenAction : IAction
+{
+    public ClearScreenAction() { }
+    public string Description => "Очистить экран";
+    public string ToBatCommand() => "cls";
+}
